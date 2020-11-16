9193754df7658fccd097b327a8168ca1ec940bc6
Gaurang Gupta <Gaurang1044>
2020-11-16 13:07:03 +0530

    fileRename

--- hello.py
+++ hello.py
@@ -5,3 +5,17 @@ print('First param:'+sys.argv[1]+'#')
 print('second param:'+sys.argv[2]+'#')
 print('third param:'+sys.argv[3]+'#')
 print(type(sys.argv[3]))
+
+import os
+
+direc = './uploads/'
+x = os.listdir(direc)
+# og_name = fieldname
+og_name = x[2]
+age = 11
+gender = 0
+
+fname = direc + str(age) + '_' + str(gender) + '_' + '2341554' + '.jpg'
+if os.path.isfile(direc + fname)
+    os.remove(direc + fname)
+os.rename(direc+og_name,fname)
